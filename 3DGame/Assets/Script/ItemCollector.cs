7307120401e1a4f8190eeccf1f244045442ffa30
Kurie <flykuro211@gmail.com>
2023-04-04 21:41:57 +0700

    Collect Item

---
+++ 3DGame/Assets/Script/ItemCollector.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ItemCollector : MonoBehaviour
+{
+    private int coin = 0;
+    [SerializeField] private TMP_Text textCoins;
+    private void OnTriggerEnter(Collider other)
+    { 
+        if (other.gameObject.CompareTag("Coin"))
+        {
+            Destroy(other.gameObject);
+            coin++;
+            textCoins.text = "Coins:" + coin;
+        }
+    }
+}
